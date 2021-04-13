e8942f1a6b71c1a56b659ae1a03e54d5d90f8262
TieWay59 <tieway59@foxmail.com>
2021-04-13 19:08:08 +0800

    引入post API

--- src/main/java/com/tieway59/ttrmp/controller/PostController.java
+++ src/main/java/com/tieway59/ttrmp/controller/PostController.java
@@ -1,5 +1,34 @@
 package com.tieway59.ttrmp.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tieway59.ttrmp.domain.Post;
+import com.tieway59.ttrmp.service.PostService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PostController {
+
+    final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+
+    @GetMapping("/post/{id}")
+    public Post getPost(@PathVariable("id") int id) {
+        return postService.
+                query().
+                eq("post_id", id).
+                one();
+    }
+
+    @GetMapping("/post")
+    public Page<Post> getAllPosts(@RequestParam("page_current") int current, @RequestParam("page_size") int size) {
+        return postService.page(new Page<>(current, size));
+    }
 
 }
